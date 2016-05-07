2b668a6ead9be0a067f950ec450727f5072d1499
Michael Ting <realq86@gmail.com>
2016-05-07 16:41:56 -0700

    Header file for last commit.

---
+++ CollectionInTableAutolayoutObjC/TableViewController/CustomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewController.h
+//  CollectionInTableAutolayoutObjC
+//
+//  Created by Michael on 5/3/16.
+//  Copyright © 2016 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewController : UIViewController
+
+@end
